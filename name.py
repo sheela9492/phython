3d79386dc5cb148815e712c3c0e050b356d32f24
sheela9492 <karpagasheela@outlook.com>
2024-09-11 18:06:45 +0100

    added code

--- name.py
+++ name.py
@@ -2,3 +2,12 @@ character_name ="sheela"
 character_age = "32"
 print("My Name" + character_name +"and")
 print("My Age is" + character_age )
+name=input("Enter your name:")
+age =input("Ender your age:")
+print("Hello " + name + " ! your age "+ age)
+num1=input("Enter a number: ")
+num2=input("Enter a number: ")
+result1=float(num1)+float(num2)
+print(result1)
+result2=int(num1)+int(num2)
+print(result2)
